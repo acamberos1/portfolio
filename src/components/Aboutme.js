2cb098197f3cef2bd4d208721ea7074757bd1470
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-01-26 17:10:15 -0800

    altered self picture size

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,9 +1,14 @@
 import React from "react";
+import myPic from "../Photo-media/me.png"
 
 function Aboutme () {
 return (
   <>
+  
   <div class = "px-1 mt-40 mb-9 text-center text-2xl">
+  <div class="mx-auto">
+      <img src = {myPic} class="max-w-sm mx-auto rounded-full shadow-2xl object-right" alt="Drawing of Athian's avatar"/>
+    </div>
     <h2 class="text-center text-5xl tracking-tight font-extrabold text-white mb-10">About Me</h2>
     <p>Originating from Arizona I didn't have an idea of what career to pursue when I set out into the work field. After taking on a few different jobs I thought that the best path would be to get a degree in college and learn the fundamentals coding. I succesfully got a degree in Informatics, and attended a coding camp called Learn Academy for hands on experience for coding. Now I reside in San Diego looking for an opportunity to find employment in web development, and become proficient in front and back end development.</p>
   </div>
